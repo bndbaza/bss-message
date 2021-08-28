0d094f3dd9de600fe37849a78959003c9b5cbd2b
viktor <bnd-baza@mail.ru>
2021-08-29 00:44:17 +0800

    start

---
+++ bss/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Entry(Base):
+    __tablename__ = 'Entry'
+    Id = Column(Integer, primary_key=True)
+    RegNumber = Column(String(15),)
+    Date = Column(Date)
+    Organ = Column(String(100))
+    Address = Column(String(100))
+    Content = Column(String(500))
+    Sender = Column(String(200))
+    Wey = Column(String(200))
+
+
---
+++ core/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+BASE_URL = 'mysql+pymysql://root:35739517@192.168.0.51:3307/InOut2'
+
+engine = create_engine(BASE_URL)
+Session = sessionmaker(bind=engine)
+
